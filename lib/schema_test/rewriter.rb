67463804b296c79ccfc46b2918210403bbb9a138
James Adam <james@lazyatom.com>
2026-05-07 21:31:42 +0100

    Apply rubocop autocorrect to the rewriter
    String concatenation to interpolation, group attr_readers, drop the
    unused block argument, prefer endless range slicing, and remove the
    trailing comma after the optional disable_rubocop array element. No
    behaviour change; all rewriter specs still pass.

--- lib/schema_test/rewriter.rb
+++ lib/schema_test/rewriter.rb
@@ -17,7 +17,7 @@ module SchemaTest
 
     def output
       current_offset = 0
-      line_indexes_with_schemas.sort_by { |(line,_)| line }.each do |index, method, name, version, location, expected_schema|
+      line_indexes_with_schemas.sort_by { |(line, _)| line }.each do |index, method, name, version, location, expected_schema|
         start_index = index + current_offset
         if lines[start_index - 1].match?(/#{DISABLE_RUBOCOP_COMMENT}/)
           lines.delete_at(start_index - 1)
@@ -26,7 +26,7 @@ module SchemaTest
         end
 
         if lines[start_index] =~ /#{OPENING_COMMENT}/
-          end_index = start_index + lines[start_index..-1].find_index { |line| line =~ /#{CLOSING_COMMENT}\s*\z/ }
+          end_index = start_index + lines[start_index..].find_index { |line| line =~ /#{CLOSING_COMMENT}\s*\z/ }
           lines.delete_at(end_index + 1) if lines[end_index + 1]&.match?(/#{ENABLE_RUBOCOP_COMMENT}/)
           json_variable_name = lines[start_index + 1].strip.gsub(/,\z/, '')
         else
@@ -36,24 +36,24 @@ module SchemaTest
 
         original_method_definition_length = end_index - start_index
         start_indent = lines[start_index].match(/\A(\s*)/)[0].length
-        (end_index - start_index + 1).times { |i| lines.delete_at(start_index) }
+        (end_index - start_index + 1).times { |_i| lines.delete_at(start_index) }
 
         inner_indent = start_indent + 2
         name_lines = SchemaTest::PrettyPrinter.format(name, indent: inner_indent).split("\n")
-        name_lines[-1] = name_lines[-1] + ','
+        name_lines[-1] = "#{name_lines[-1]},"
         options_lines = SchemaTest::PrettyPrinter.format(
           { version: version, schema: expected_schema },
           indent: inner_indent
         ).split("\n")
 
-        expanded_schema_lines = [(' ' * inner_indent) + json_variable_name + ',', *name_lines, *options_lines]
+        expanded_schema_lines = ["#{' ' * inner_indent}#{json_variable_name},", *name_lines, *options_lines]
 
         method_string = [
           disable_rubocop ? (' ' * start_indent) + DISABLE_RUBOCOP_COMMENT : nil,
           (' ' * start_indent) + method.to_s + "( #{OPENING_COMMENT} from #{location}",
           *expanded_schema_lines,
           (' ' * start_indent) + ") #{CLOSING_COMMENT}",
-          disable_rubocop ? (' ' * start_indent) + ENABLE_RUBOCOP_COMMENT : nil,
+          disable_rubocop ? (' ' * start_indent) + ENABLE_RUBOCOP_COMMENT : nil
         ].compact
 
         method_string.reverse.each { |line| lines.insert(start_index, line) }
@@ -61,12 +61,11 @@ module SchemaTest
         current_offset += method_string.count - original_method_definition_length - 1
       end
 
-      lines.compact.join("\n") + "\n"
+      "#{lines.compact.join("\n")}\n"
     end
 
     private
 
-    attr_reader :lines, :line_indexes_with_schemas
-    attr_reader :disable_rubocop
+    attr_reader :lines, :line_indexes_with_schemas, :disable_rubocop
   end
 end
